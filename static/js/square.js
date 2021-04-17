448d6db7784ca14c6ffe1044b0963bf11258e857
zack <1182725354@qq.com>
2021-04-17 17:50:56 +0800

    修复活动id无法正常传输的bug

--- static/js/square.js
+++ static/js/square.js
@@ -287,6 +287,7 @@ function getRightSide() {
                 content2 = '<h3>在<strong> ' + response.year + ' </strong>你已经<br/>参加活动\
                         <strong> <span>' + response.activityJoined + '</span> </strong>次</h3>\
                         <h3>发布活动 <strong> <span>' + response.activityPosted + '</span> </strong>次</h3>\
+                        <h3>参与帮助 <strong> <span>' + response.helpJoined + '</span> </strong>次</h3>\
                         <h3>发布帮助 <strong> <span> ' + response.helpPosted + '</span> </strong>次</h3>';
                 $("#profileUserInfo").append(content1);
                 $("#profileRecord").append(content2);
@@ -297,17 +298,6 @@ function getRightSide() {
         }
     });
 };
-
-// atvt1 = '<div id="'+ response[i].actiObject.aid +'" class="item-wrap" style="background-color:'+ bgcolor[i%4] +';">\
-// <img src="'+ serverAddress + response[i].pic +'" alt="pick">\
-// <div class="item-imfo">\
-//     <p>名称: <span class="atvt-name">'+ response[i].actiObject.name +'</span></p>\
-//     <p>时间: <span class="atvt-start">'+ response[i].startTimeFormed +'</span>~<span class="atvt-end">'+ response[i].endTimeFormed +'</span></p>\
-//     <p>地点: <span class="atvt-lctn">'+ response[i].actiObject.location +'</span></p>\
-//     <p>所需人数|已有人数:: <span class="atvt-req">'+ response[i].needNum+'|'+response[i].peopleNum +'</span></p>\
-//     <p>播放录音: <span class="atvt-play"></span><a href="'+serverAddress+response[i].audio +'"><svg t="1614866250411" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1160" width="17" height="17"><path d="M473.088 125.44L256 256H52.224C23.552 256 0 279.552 0 308.224V716.8c0 28.16 23.04 51.2 51.2 51.2h204.8l217.088 130.56c16.896 10.24 38.912-2.048 38.912-22.016V147.456c0-19.968-21.504-32.256-38.912-22.016zM699.904 320.512c-20.992-18.944-53.248-17.408-72.192 3.584-18.944 20.992-17.408 53.248 3.584 72.192 0.512 0.512 58.368 54.784 58.368 121.344 0 37.888-19.456 74.752-58.368 110.08-20.992 18.944-22.528 51.2-3.584 72.192 10.24 11.264 24.064 16.896 37.888 16.896 12.288 0 24.576-4.608 34.304-13.312 61.44-55.296 92.16-117.76 92.16-185.856 0-112.64-88.576-193.536-92.16-197.12z" fill="" p-id="1161"></path><path d="M853.504 166.4c-20.992-18.944-53.248-16.896-72.192 4.096-18.944 20.992-16.896 53.248 4.096 72.192 1.536 1.024 135.68 122.88 135.68 280.576 0 90.624-45.568 177.152-135.68 257.536-20.992 18.944-23.04 51.2-4.096 72.192 10.24 11.264 24.064 16.896 38.4 16.896 12.288 0 24.576-4.096 34.304-12.8 112.64-100.864 169.984-212.992 169.984-333.824-1.024-202.752-163.84-350.208-170.496-356.864z" fill="" p-id="1162"></path></svg></a></p>\
-//     <p>具体信息: <span class="atvt-imfo">'+ response[i].actiObject.intro +'</span></p>\
-//     <div class="op-item">'
 
 atvt2 = '</div></div></div>'
 
